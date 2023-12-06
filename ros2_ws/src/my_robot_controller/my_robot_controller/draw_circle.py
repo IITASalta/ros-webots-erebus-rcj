53ff993593dc2eec6dc264d46604bcecaf460f9c
ale <aledeum.saf@gmail.com>
2023-12-06 16:18:15 -0300

    made draw circle publish to tutrlebot topci

--- ros2_ws/src/my_robot_controller/my_robot_controller/draw_circle.py
+++ ros2_ws/src/my_robot_controller/my_robot_controller/draw_circle.py
@@ -7,15 +7,15 @@ from geometry_msgs.msg import Twist
 class DrawCircleNode(Node):
     def __init__(self):
         super().__init__("draw_circle")
-        self.cmd_vel_pub_ = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
+        self.cmd_vel_pub_ = self.create_publisher(Twist, "/cmd_vel", 10)
         self.create_timer(.5, self.send_velocity_command)
         self.get_logger().info("Draw circle node has been started")
 
 
     def send_velocity_command(self):
         msg = Twist()
-        msg.linear.x = 1.
-        msg.angular.z = 1.
+        msg.linear.x = .2
+        msg.angular.z = .2
         self.cmd_vel_pub_.publish(msg)
 
 def main(args=None):
